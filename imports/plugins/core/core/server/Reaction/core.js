c5147b432167bf80e0ab0e076fda685ae455fa70
Pat Newell <pnewell+git@renttherunway.com>
2018-07-17 08:59:25 -0400

    Two Query Approach to Prioritizing Primary Shop

--- imports/plugins/core/core/server/Reaction/core.js
+++ imports/plugins/core/core/server/Reaction/core.js
@@ -414,18 +414,25 @@ export default {
    * @method
    * @memberof Core
    * @summary returns the shop which should be used given the current domain
+   * @return {StringId} shopId for the current domain
    */
   getShopIdByDomain() {
     const domain = this.getDomain();
+    const primaryShop = this.getPrimaryShop();
+
+    // in cases where the domain could match multiple shops, we first check
+    // whether the primaryShop matches the current domain. If so, we give it
+    // priority
+    if (primaryShop.domains.includes(domain)) {
+      return primaryShop._id;
+    }
+
     const shop = Shops.find({
       domains: domain
     }, {
       limit: 1,
       fields: {
         _id: 1
-      },
-      orderby: {
-        merchantShops: -1 // give preference to PrimaryShop if multiple results
       }
     }).fetch()[0];
 
