4d023d1616c74c044e2407c85ebdd39c72f993bd
griggheo <grig@gheorghiu.net>
2018-07-18 17:04:12 -0700

    manifest file and cloudformation templates for VPC and ECS cluster creation

---
+++ devops/aws/ecs/create-ecs-stack.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# Set AWS_PROFILE environment variable if desired.
+
+function join_strings {
+    local IFS="$1";
+    shift;
+    echo "$*";
+}
+
+PARENT_DIR="$(dirname "$(pwd)")"
+MANIFEST_FILE="${PARENT_DIR}/manifest.yaml"
+
+if [ ! -f ${MANIFEST_FILE} ]; then
+    echo "Manifest file not found!"
+    exit 1
+fi
+
+APP_NAME=$(/usr/local/bin/yq r $MANIFEST_FILE "application.name")
+ENV_NAME=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].name")
+VPC_STACK_NAME="${APP_NAME}-${ENV_NAME}-vpc"
+
+# Create an EC2 keypair that will be referenced in the CF template
+# make sure the key doesn't already exist
+KEYPAIR_NAME=$STACK_NAME
+aws ec2 describe-key-pairs --key-names $KEYPAIR_NAME > /dev/null 2>&1
+if [ $? != 0 ]; then
+  aws ec2 create-key-pair --key-name $KEYPAIR_NAME | jq -r ".KeyMaterial" > ~/.ssh/${KEYPAIR_NAME}.pem
+  chmod 600 ~/.ssh/${KEYPAIR_NAME}.pem
+fi
+
+CLUSTER_COUNT=$(/usr/local/bin/yq r $MANIFEST_FILE 'application.environments[0].ecs.clusters.*.type' | wc -l | xargs)
+
+for i in $( seq 0 $((CLUSTER_COUNT-1)))
+do
+	CLUSTER_TYPE=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].ecs.clusters[$i].type")
+	STACK_NAME="${APP_NAME}-${ENV_NAME}-${CLUSTER_TYPE}"
+	CLUSTER_NAME=$STACK_NAME
+	CLUSTER_SIZE=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].ecs.clusters[$i].cluster_size")
+	INSTANCE_TYPE=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].ecs.clusters[$i].instance_type")
+
+	cloudformation_template_file="file://${CLUSTER_TYPE}.yaml"
+
+	stack_parameters=$(join_strings " " \
+	  "ParameterKey=CloudFormationVPCStackName,ParameterValue=$VPC_STACK_NAME" \
+	  "ParameterKey=ClusterName,ParameterValue=$CLUSTER_NAME" \
+	  "ParameterKey=InstanceType,ParameterValue=$INSTANCE_TYPE" \
+	  "ParameterKey=ClusterSize,ParameterValue=$CLUSTER_SIZE")
+
+	stack_tags=$(join_strings " " \
+	  "Key=${APP_NAME}/environment,Value=$ENV_NAME" \
+	  "Key=${APP_NAME}/app,Value=$APP_NAME" \
+	  "Key=${APP_NAME}/app-role,Value=$CLUSTER_TYPE" \
+	  "Key=${APP_NAME}/billing,Value=architecture" \
+	  "Key=${APP_NAME}/created-by,Value=cloudformation")
+
+	aws cloudformation create-stack \
+	  --stack-name $STACK_NAME \
+	  --parameters $stack_parameters \
+	  --template-body $cloudformation_template_file \
+	  --tags $stack_tags \
+	  --capabilities CAPABILITY_NAMED_IAM
+
+	aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
+done
---
+++ devops/aws/vpc/create-vpc-stack.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# Set AWS_PROFILE environment variable if desired.
+
+function join_strings {
+    local IFS="$1";
+    shift;
+    echo "$*";
+}
+
+PARENT_DIR="$(dirname "$(pwd)")"
+MANIFEST_FILE="${PARENT_DIR}/manifest.yaml"
+
+if [ ! -f ${MANIFEST_FILE} ]; then
+    echo "Manifest file not found!"
+    exit 1
+fi
+
+APP_NAME=$(/usr/local/bin/yq r $MANIFEST_FILE application.name)
+ENV_NAME=$(/usr/local/bin/yq r $MANIFEST_FILE application.environments[0].name)
+STACK_NAME="${APP_NAME}-${ENV_NAME}-vpc"
+
+VPC_CIDR=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].vpc.vpc_cidr")
+PRIVATE_AZ_A_CIDR=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].vpc.private_az_a_cidr")
+PRIVATE_AZ_B_CIDR=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].vpc.private_az_b_cidr")
+PRIVATE_AZ_C_CIDR=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].vpc.private_az_c_cidr")
+PUBLIC_AZ_A_CIDR=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].vpc.public_az_a_cidr")
+PUBLIC_AZ_B_CIDR=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].vpc.public_az_b_cidr")
+PUBLIC_AZ_C_CIDR=$(/usr/local/bin/yq r $MANIFEST_FILE "application.environments[0].vpc.public_az_c_cidr")
+
+
+cloudformation_template_file="file://vpc.yaml"
+
+stack_parameters=$(join_strings " " \
+  "ParameterKey=VpcCidrParam,ParameterValue=$VPC_CIDR" \
+  "ParameterKey=PrivateAZASubnetBlock,ParameterValue=$PRIVATE_AZ_A_CIDR" \
+  "ParameterKey=PrivateAZBSubnetBlock,ParameterValue=$PRIVATE_AZ_B_CIDR" \
+  "ParameterKey=PrivateAZCSubnetBlock,ParameterValue=$PRIVATE_AZ_C_CIDR" \
+  "ParameterKey=PublicAZASubnetBlock,ParameterValue=$PUBLIC_AZ_A_CIDR" \
+  "ParameterKey=PublicAZBSubnetBlock,ParameterValue=$PUBLIC_AZ_B_CIDR" \
+  "ParameterKey=PublicAZCSubnetBlock,ParameterValue=$PUBLIC_AZ_C_CIDR") 
+
+stack_tags=$(join_strings " " \
+  "Key=${APP_NAME}/environment,Value=$ENV_NAME" \
+  "Key=${APP_NAME}/app,Value=$APP_NAME" \
+  "Key=${APP_NAME}/app-role,Value=network" \
+  "Key=${APP_NAME}/billing,Value=architecture" \
+  "Key=${APP_NAME}/created-by,Value=cloudformation")
+
+aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --parameters $stack_parameters \
+  --template-body $cloudformation_template_file \
+  --tags $stack_tags \
+  --capabilities CAPABILITY_NAMED_IAM
+
+aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
